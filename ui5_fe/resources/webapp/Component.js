e6af04f551908b9f002b44e860bf0056b2830f46
alex <alexey.myakinky@dev4hana.ru>
2022-07-25 23:41:33 +0300

    remove ui5_fe stub module

--- ui5_fe/resources/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-jQuery.sap.declare("ru.fitrepublic.ui5_fe.Component");
-sap.ui.getCore().loadLibrary("sap.ui.generic.app");
-jQuery.sap.require("sap.ui.generic.app.AppComponent");
-
-sap.ui.generic.app.AppComponent.extend("ru.fitrepublic.ui5_fe.Component", {
-	metadata: {
-		"manifest": "json"
-	}
-});

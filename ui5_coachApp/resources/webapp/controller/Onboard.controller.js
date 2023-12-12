4d42a7cf07b02117247572196f07093a407c0ac2
alex <alexey.myakinky@dev4hana.ru>
2023-12-12 21:55:50 +0100

    barcode scanner for coach app

--- ui5_coachApp/resources/webapp/controller/Onboard.controller.js
+++ ui5_coachApp/resources/webapp/controller/Onboard.controller.js
@@ -17,24 +17,8 @@ sap.ui.define([ "ru/fitrepublic/shared/controller/BaseController", "ru/fitrepubl
 		},
 		
 		scanQRCode:function(){
-			var options = {
-				preferFrontCamera: false, // iOS and Android
-				showFlipCameraButton: true, // iOS and Android
-				showTorchButton: true, // iOS and Android
-				torchOn: true, // Android, launch with the torch switched on (if available)
-				prompt: this.geti18n('onboardQrScanPrompt'), // Android
-				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				formats: "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
-				orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-				disableAnimations: true // iOS
-			};
-			return new Promise(function(resolve,reject){
-				if (typeof cordova === "undefined" || !cordova.plugins.barcodeScanner) resolve({name:'dummy',authToken:'123'});
-				cordova.plugins.barcodeScanner.scan(function(data){
-					if (data.text.charCodeAt(0) === 0xFEFF) resolve(JSON.parse(data.text.slice(1))); // utf BOM
-					else resolve(JSON.parse(data.text));
-				}, reject, options);
-			});
+			if (window.startScan) return window.startScan()
+			return Promise.resolve({name:'dummy',authToken:'123'});
 		},
 		
 		confirmName:function(qrData){
